78c991c64c579e88266e1e22a854433a3d6376f5
jahangir <jahangiralompial@gmail.com>
2020-09-04 17:54:15 +0600

    Meetings App

---
+++ meetings/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Meeting, Room
+
+admin.site.register(Meeting)
+admin.site.register(Room)
---
+++ meetings/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from datetime import time
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} on {self.room_number} at {self.floor}th Floor"
+
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.date} on {self.start_time}"
