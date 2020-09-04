811d4d481127070169d0b2aa69bb09bac0cfd9ff
jahangir <jahangiralompial@gmail.com>
2020-09-04 12:28:38 +0600

    Website App

--- website/views.py
+++ website/views.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to meeting Planner')
+
+
+def about(request):
+    return HttpResponse(f"We are the developers of meeting planner app {datetime.now()}")
